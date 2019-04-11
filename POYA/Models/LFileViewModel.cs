dac6637304a632f8c3fbf09b69f9273ae6119fb7
Larry Wei <2034643712@qq.com>
2019-04-11 12:21:30 +0800

    Test

--- POYA/Models/LFileViewModel.cs
+++ POYA/Models/LFileViewModel.cs
@@ -5,10 +5,24 @@ using System.Threading.Tasks;
 
 namespace POYA.Models
 {
-    public class LFile  //  ViewModel
+    public class LUserFile  //  ViewModel
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        /// <summary>
+        /// The MD5 string, it equal the file name in specified directory
+        /// </summary>
         public string MD5 { get; set; }
         public string UserId { get; set; }
+        /// <summary>
+        /// It is shared if SharedCode isn't Guid.Empty
+        /// </summary>
+        public Guid SharedCode { get; set; } = Guid.Empty;
+    }
+    public class LFile
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public string Upload_UserId { get; set; }
+        public DateTimeOffset DOUpload { get; set; } = DateTimeOffset.Now;
+        public string MD5 { get; set; }
     }
 }
