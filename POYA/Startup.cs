a82933b70ea95a27b822e7886a8a4fe566c2f5b3
1arrywei <2034643712@qq.com>
2019-06-19 20:53:02 +0800

    kk

--- POYA/Startup.cs
+++ POYA/Startup.cs
@@ -52,7 +52,7 @@ namespace POYA
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             #region
