0ebe4c34b35611134697f1b8fc4c7a27ff049582
Larry Wei <2034643712@qq.com>
2019-06-19 17:45:15 +0800

    🐄

---
+++ POYA/Areas/XSchool/Models/LSchoolViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POYA.Areas.XSchool.Models
+{
+
+    public class LSchool    //  ViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string AdminId { get; set; }
+        public string CoverMD5 { get; set; }
+        public string Intro { get; set; }
+        public DateTimeOffset DORegistering { get; set; }
+        public bool IsVerified { get; set; } = false;
+        public string Motto { get; set; }
+    }
+}
