d5e7e18b19c8c96a7909fc5af4f50122c8f1e7d5
Larry Wei <2034643712@qq.com>
2019-06-20 22:06:27 +0800

    🤞

--- POYA/Areas/XSchool/Controllers/LSchoolAdminsController.cs
+++ POYA/Areas/XSchool/Controllers/LSchoolAdminsController.cs
@@ -2,31 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ganss.XSS;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using POYA.Areas.EduHub.Controllers;
 using POYA.Areas.XSchool.Models;
+using POYA.Areas.XUserFile.Controllers;
 using POYA.Data;
+using POYA.Unities.Helpers;
 
 namespace POYA.Areas.XSchool.Controllers
 {
     [Area("XSchool")]
     public class LSchoolAdminsController : Controller
     {
+        #region     DI
+        private readonly IHostingEnvironment _hostingEnv;
+        private readonly IStringLocalizer<Program> _localizer;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEmailSender _emailSender;
         private readonly ApplicationDbContext _context;
-
-        public LSchoolAdminsController(ApplicationDbContext context)
+        private readonly X_DOVEHelper _x_DOVEHelper;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly HtmlSanitizer _htmlSanitizer;
+        private readonly MimeHelper _mimeHelper;
+        private readonly XUserFileHelper _xUserFileHelper;
+        public LSchoolAdminsController(
+            MimeHelper mimeHelper,
+            HtmlSanitizer htmlSanitizer,
+            SignInManager<IdentityUser> signInManager,
+            X_DOVEHelper x_DOVEHelper,
+            RoleManager<IdentityRole> roleManager,
+           IEmailSender emailSender,
+           UserManager<IdentityUser> userManager,
+           ApplicationDbContext context,
+           IHostingEnvironment hostingEnv,
+           IStringLocalizer<Program> localizer)
         {
+            _htmlSanitizer = htmlSanitizer;
+            _hostingEnv = hostingEnv;
+            _localizer = localizer;
             _context = context;
+            _userManager = userManager;
+            _emailSender = emailSender;
+            _roleManager = roleManager;
+            _x_DOVEHelper = x_DOVEHelper;
+            _signInManager = signInManager;
+            _mimeHelper = mimeHelper;
+            _xUserFileHelper = new XUserFileHelper();
         }
+        #endregion
+
+
+        #region 
 
         // GET: XSchool/LSchoolAdmins
+        #endregion
         public async Task<IActionResult> Index()
         {
             return View(await _context.LSchoolAdmin.ToListAsync());
         }
 
+        #region 
+
         // GET: XSchool/LSchoolAdmins/Details/5
+        #endregion
         public async Task<IActionResult> Details(Guid? id)
         {
             if (id == null)
@@ -44,17 +91,23 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolAdmin);
         }
 
+        #region 
+
         // GET: XSchool/LSchoolAdmins/Create
+        #endregion
         public IActionResult Create()
         {
             return View();
         }
+
+        #region 
 
         // POST: XSchool/LSchoolAdmins/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        #endregion
         public async Task<IActionResult> Create([Bind("Id,UserId,LSchoolId,DOConfirmation,IsConfirmed,IsCancelled")] LSchoolAdmin lSchoolAdmin)
         {
             if (ModelState.IsValid)
@@ -67,7 +120,10 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolAdmin);
         }
 
+        #region 
+
         // GET: XSchool/LSchoolAdmins/Edit/5
+        #endregion
         public async Task<IActionResult> Edit(Guid? id)
         {
             if (id == null)
@@ -83,11 +139,14 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolAdmin);
         }
 
+        #region 
+
         // POST: XSchool/LSchoolAdmins/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        #endregion
         public async Task<IActionResult> Edit(Guid id, [Bind("Id,UserId,LSchoolId,DOConfirmation,IsConfirmed,IsCancelled")] LSchoolAdmin lSchoolAdmin)
         {
             if (id != lSchoolAdmin.Id)
@@ -118,7 +177,10 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolAdmin);
         }
 
+        #region 
+
         // GET: XSchool/LSchoolAdmins/Delete/5
+        #endregion
         public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
@@ -136,9 +198,12 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolAdmin);
         }
 
+        #region 
+
         // POST: XSchool/LSchoolAdmins/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        #endregion
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             var lSchoolAdmin = await _context.LSchoolAdmin.FindAsync(id);
--- POYA/Areas/XSchool/Controllers/LSchoolNewsController.cs
+++ POYA/Areas/XSchool/Controllers/LSchoolNewsController.cs
@@ -2,31 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ganss.XSS;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 using POYA.Areas.XSchool.Models;
+using POYA.Areas.XUserFile.Controllers;
 using POYA.Data;
+using POYA.Unities.Helpers;
 
 namespace POYA.Areas.XSchool.Controllers
 {
     [Area("XSchool")]
     public class LSchoolNewsController : Controller
     {
-        private readonly ApplicationDbContext _context;
 
-        public LSchoolNewsController(ApplicationDbContext context)
+
+        #region     DI
+        private readonly IHostingEnvironment _hostingEnv;
+        private readonly IStringLocalizer<Program> _localizer;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ApplicationDbContext _context;
+        private readonly X_DOVEHelper _x_DOVEHelper;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly HtmlSanitizer _htmlSanitizer;
+        private readonly MimeHelper _mimeHelper;
+        private readonly XUserFileHelper _xUserFileHelper;
+        public LSchoolNewsController(
+            MimeHelper mimeHelper,
+            HtmlSanitizer htmlSanitizer,
+            SignInManager<IdentityUser> signInManager,
+            X_DOVEHelper x_DOVEHelper,
+            RoleManager<IdentityRole> roleManager,
+           IEmailSender emailSender,
+           UserManager<IdentityUser> userManager,
+           ApplicationDbContext context,
+           IHostingEnvironment hostingEnv,
+           IStringLocalizer<Program> localizer)
         {
+            _htmlSanitizer = htmlSanitizer;
+            _hostingEnv = hostingEnv;
+            _localizer = localizer;
             _context = context;
+            _userManager = userManager;
+            _emailSender = emailSender;
+            _roleManager = roleManager;
+            _x_DOVEHelper = x_DOVEHelper;
+            _signInManager = signInManager;
+            _mimeHelper = mimeHelper;
+            _xUserFileHelper = new XUserFileHelper();
         }
+        #endregion
+
+        #region 
 
         // GET: XSchool/LSchoolNews
+        #endregion
         public async Task<IActionResult> Index()
         {
             return View(await _context.LSchoolNews.ToListAsync());
         }
 
+        #region 
+
         // GET: XSchool/LSchoolNews/Details/5
+        #endregion
         public async Task<IActionResult> Details(Guid? id)
         {
             if (id == null)
@@ -44,17 +90,23 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolNews);
         }
 
+        #region 
+
         // GET: XSchool/LSchoolNews/Create
+        #endregion
         public IActionResult Create()
         {
             return View();
         }
+
+        #region 
 
         // POST: XSchool/LSchoolNews/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        #endregion
         public async Task<IActionResult> Create([Bind("Id,Title,Content,LSchoolId,PublisherId,DOPublishing,DOModifying")] LSchoolNews lSchoolNews)
         {
             if (ModelState.IsValid)
@@ -67,7 +119,10 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolNews);
         }
 
+        #region 
+
         // GET: XSchool/LSchoolNews/Edit/5
+        #endregion
         public async Task<IActionResult> Edit(Guid? id)
         {
             if (id == null)
@@ -83,11 +138,14 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolNews);
         }
 
+        #region 
+
         // POST: XSchool/LSchoolNews/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        #endregion
         public async Task<IActionResult> Edit(Guid id, [Bind("Id,Title,Content,LSchoolId,PublisherId,DOPublishing,DOModifying")] LSchoolNews lSchoolNews)
         {
             if (id != lSchoolNews.Id)
@@ -118,7 +176,10 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolNews);
         }
 
+        #region 
+
         // GET: XSchool/LSchoolNews/Delete/5
+        #endregion
         public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
@@ -136,9 +197,12 @@ namespace POYA.Areas.XSchool.Controllers
             return View(lSchoolNews);
         }
 
+        #region 
+
         // POST: XSchool/LSchoolNews/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        #endregion
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             var lSchoolNews = await _context.LSchoolNews.FindAsync(id);
