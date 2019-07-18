d152835e0a01337986898df0c7e5b7477ce451a5
1arrywei <2034643712@qq.com>
2019-07-18 12:11:19 +0800

    Optimizing X_DOVERequestCultureProvider.cs

--- POYA/Unities/Helpers/X_DOVERequestCultureProvider.cs
+++ POYA/Unities/Helpers/X_DOVERequestCultureProvider.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace POYA.Unities.Helpers
 {
     public class X_DOVERequestCultureProvider : RequestCultureProvider
@@ -11,8 +15,11 @@ namespace POYA.Unities.Helpers
         public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
         {
             var CULTURE_String="CULTURE";
-            var CultureCookie = httpContext.Request.Cookies[CULTURE_String]?.ToString() ?? ""; 
-            if (string.IsNullOrWhiteSpace(CultureCookie))
+            var CultureCookie = httpContext.Request.Cookies[CULTURE_String]?.ToString() ?? "";    
+            var _SupportedUICultures = httpContext.RequestServices
+                                        .GetService<IOptions<RequestLocalizationOptions>>()
+                                        .Value.SupportedUICultures.Select(c => c.Name).ToList();
+            if (string.IsNullOrWhiteSpace(CultureCookie)|| !_SupportedUICultures.Contains(CultureCookie))
             {
                 CultureCookie = "zh-Hans";
                 httpContext.Response.Cookies.Append(key:CULTURE_String, value:CultureCookie, options: new CookieOptions() { Expires = DateTime.Now.AddYears(1) });
