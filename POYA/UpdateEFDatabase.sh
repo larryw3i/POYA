48f911790d3db3a44b82744f081c393e50fcdade
Larry Wei <larryw3i@163.com>
2019-08-02 00:15:57 +0800

    ff

--- POYA/UpdateEFDatabase.sh
+++ POYA/UpdateEFDatabase.sh
@@ -6,10 +6,10 @@ _cd="cd "$_ScriptRoot
 _dotnet_ef_migrations_add="dotnet ef migrations add "$_DateTime
 _dotnet_ef_database_update="dotnet ef database update"
 _ok=">>>>\\(^_^)//\\(^_^)//\\(^_^)//\\(^_^)//<<<<"
-echo $_L$_cd
-${_cd}
-echo $_L$_dotnet_ef_migrations_add
-${_dotnet_ef_migrations_add}
-echo $_L$_dotnet_ef_database_update
-${_dotnet_ef_database_update}
-echo $_L$_ok
+echo	$_L$_cd
+		${_cd}
+echo	$_L$_dotnet_ef_migrations_add
+		${_dotnet_ef_migrations_add}
+echo	$_L$_dotnet_ef_database_update
+		${_dotnet_ef_database_update}
+echo	$_L$_ok
