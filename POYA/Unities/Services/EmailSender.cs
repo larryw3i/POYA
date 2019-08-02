9400edd906dcf98b64b230140cb52aade0a3e48b
larryw3i <larryw3i@163.com>
2019-08-03 02:48:17 +0800

    dd

--- POYA/Unities/Services/EmailSender.cs
+++ POYA/Unities/Services/EmailSender.cs
@@ -27,18 +27,20 @@ namespace POYA.Unities.Services
             var password = _configuration["EmailSender:password"];//     (string)jsonObject[nameof(EmailSender)]["password"];
             var port = Convert.ToInt32(_configuration["EmailSender:port"]);//    (short)jsonObject[nameof(EmailSender)]["port"];
             var enableSsl = Convert.ToBoolean(_configuration["EmailSender:enableSsl"]);
-            var smtpClient = new SmtpClient(host: host, port: port)
+            using (var smtpClient = new SmtpClient(host: host, port: port)
             {
                 EnableSsl = enableSsl,
                 Credentials = new System.Net.NetworkCredential(userName: userName, password: password),
-            };
-            var mailMessage = new MailMessage(subject: subject, body: htmlMessage, from: userName, to: email)
+            })
             {
-                IsBodyHtml = true,
-                SubjectEncoding = Encoding.UTF8,
-                BodyEncoding = Encoding.UTF8
-            };
-            return smtpClient.SendMailAsync(mailMessage);
+                var mailMessage = new MailMessage(subject: subject, body: htmlMessage, from: userName, to: email)
+                {
+                    IsBodyHtml = true,
+                    SubjectEncoding = Encoding.UTF8,
+                    BodyEncoding = Encoding.UTF8
+                };
+                return smtpClient.SendMailAsync(mailMessage);
+            }
             //throw new NotImplementedException();
         }
     }
